812af15b2adaef44d7c4347491c142ae66816a0b
mjblaha22 <mjblaha22@gmail.com>
2020-09-23 10:23:46 -0400

    restart

---
+++ AlumniApp2/Alumni_Accounts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AlumniAccountsConfig(AppConfig):
+    name = 'Alumni_Accounts'
---
+++ AlumniApp2/Alumni_Accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+# Create your models here.
